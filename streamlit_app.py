1ee85e8afa661958ba6cdd366d317b13e6b25421
Steven Chacón <stechacon@estudiantec.cr>
2025-08-19 02:19:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,8 @@ st.write('The name on your smoothie will be : ' ,name_on_order)
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake");
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-
-
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingridients:'
